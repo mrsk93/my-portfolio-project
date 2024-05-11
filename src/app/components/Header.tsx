b5237bc27606ac91046cf52325ac8baba2bb7447
Sumit Chalotra <kumar.sumit9981@gmail.com>
2024-05-12 02:29:38 +0530

    header / navbar made responsive

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,22 +3,47 @@ import Link from "next/link";
 import React from 'react'
 function Header() {
   return (
-    <div className='header-section flex'>
-        <div className="logo-section">
-            <Link href="#">
-                <Image className="logo" src={'/logo.png'} alt={"Logo"} fill={true}/>
-            </Link>
-        </div>
-        <nav className="nav-section">
-            <ul className="nav-list flex">
-                <li className="nav-list-item"><Link href="#skills">Skills</Link></li>
-                <li className="nav-list-item"><Link href="#experience">Experience</Link></li>
-                <li className="nav-list-item"><Link href="#projects">Projects</Link></li>
-                <li className="nav-list-item"><Link href="#contactMe">Contact Me</Link></li>
+    <header>
+      <div className="header-section">
+        <div className="nav-bar">
+          <nav className="nav-section">
+            <div className="logo-section">
+              <Link href="#">
+                <Image
+                  className="logo"
+                  src={"/logo.png"}
+                  alt={"Logo"}
+                  fill={true}
+                />
+              </Link>
+            </div>
+            <ul className="nav-list">
+              <li className="nav-list-item">
+                <Link href="#skills">Skills</Link>
+              </li>
+              <li className="nav-list-item">
+                <Link href="#experience">Experience</Link>
+              </li>
+              <li className="nav-list-item">
+                <Link href="#projects">Projects</Link>
+              </li>
+              <li className="nav-list-item">
+                <Link href="#contactMe">Contact Me</Link>
+              </li>
             </ul>
-        </nav>
-    </div>
-  )
+            <button className="burger-menu" type="button">
+              <Image
+                className="burger-menu"
+                src={"/burger-menu.svg"}
+                alt={"burger-menu"}
+                fill={true}
+              />
+            </button>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header
